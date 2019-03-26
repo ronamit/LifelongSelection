7690073c075ab668b40c54a8270aea5c1048d241
ronamit <Was21235>
2019-03-26 11:55:11 +0200

    update

--- main_sin.py
+++ main_sin.py
@@ -8,6 +8,7 @@ matplotlib.rcParams.update({'lines.linewidth': 2})
 matplotlib.rcParams['pdf.fonttype'] = 42
 matplotlib.rcParams['ps.fonttype'] = 42
 
+import torch
 
 # -------------------------------------------------------------------------------------------
 #  Create data
@@ -15,6 +16,8 @@ matplotlib.rcParams['ps.fonttype'] = 42
 
 # TODO: Random seed
 
+device_id = 0
+torch.cuda.device(device_id)
 
 # generate inputs
 m = 1000  # data-set size
@@ -28,8 +31,7 @@ class SineTask():
         self.freq = freq
         self.x = self.x_range[0] + np.random.rand(m) * self.x_range[1]
         self.y = np.empty_like(self.x)
-        for i_point in range(m):
-            self.y[i_point] = self.amplitude * np.sin(self.x[i_point] * 2 * np.pi * self.freq)
+        self.y = self.amplitude * np.sin(self.x * 2 * np.pi * self.freq)
 
     def plot(self):
         #  Plots:
@@ -39,17 +41,34 @@ class SineTask():
         plt.ylabel('y')
         plt.show()
 
+    def learn(self, prior=None):
+        boundVal = None
+        posterior = None
+
+        Q_mu = torch.randn(1, requires_grad=True)
+        return posterior, boundVal
+
+
     # def calc_test_error(self):
 
 
 
-freq_arr = np.linspace(0.1, 0.5, 5)  # possible frequencies
-n_freqs = len(freq_arr)
-i_freq = np.random.randint(0, len(freq_arr) - 1)
-freq = freq_arr[i_freq]
+freqArr = [0.1, 0.2, 0.3, 0.4, 0.5]  # possible frequencies values
+nFreqs = len(freqArr)
+
+# Define the true distribution of freqs in the environment
+freqDist = np.array([1,1,5,1,1], dtype=float)
+freqDist = freqDist / freqDist.sum
+assert nFreqs == len(freqDist)
+
+i_freq = np.random.randint(0, len(freqArr) - 1)
+freq = freqArr[i_freq]
 
 taskData = SineTask(n_samples=1000, freq=freq)
 taskData.plot()
+
+nPriors = 5
+prior_mu = np.linspace(0.1, 0.5, nPriors)
 
 
 # Main sequence loop
