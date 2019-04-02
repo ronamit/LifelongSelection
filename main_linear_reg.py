ad898ce6ee835a09961c4e9159d7e4be2f0a1e2e
ronamit <amitron5@gmail.com>
2019-04-02 16:01:02 +0300

    update - Office PC

--- main_linear_reg.py
+++ main_linear_reg.py
@@ -20,9 +20,9 @@ torch.cuda.device(device_id)
 # -------------------------------------------------------------------------------------------
 class TaskEnvironment():
     def __init__(self):
-        self.aMean = 3
-        self.aStd = 1 # TODO: increase
-        self.noiseStd = 4.0
+        self.aMean = 4
+        self.aStd = 0.1
+        self.noiseStd = 5.0
         self.xRange = (0, 10)
 
     def generate_task(self):
@@ -56,7 +56,8 @@ class Task():
         self.get_samples(n_samples_test)
         x = trainData.x
         y = trainData.y
-        test_err = (1/n_samples_test) * (torch.norm(y - torch.matmul(x,postMu))**2 + postVar * torch.norm(x)**2)
+        test_err = (1/n_samples_test) * (torch.norm(y - torch.matmul(x, postMu))**2
+                                         + postVar * torch.norm(x)**2)
         return test_err
 
 
@@ -103,12 +104,22 @@ def run_task_learner(trainData, priorMu, priorVar):
     regFactor = np.sqrt(n_samples) / (2 * priorVar)
     matA = regFactor * torch.eye(dim) + torch.matmul(x.t(),  x)
     matB = regFactor * priorMu + torch.matmul(x.t(), y)
+    # postMu = torch.from_numpy(np.linalg.solve(matA, matB))
     postMu = torch.matmul(torch.pinverse(matA), matB)
     taskBound = (1/n_samples) * (torch.norm(matB - matA.t() * postMu)**2 + regFactor * torch.norm(postMu - priorMu)**2)
     # TODO: calculate exact bound for comparison with actual results
     return postMu, taskBound
 
-
+def run_no_prior_learner(trainData):
+    # note: we assume priorVar == postVar
+    x = trainData.x
+    y = trainData.y
+    dim = x.shape[1]
+    n_samples = trainData.n_samples
+    matA =  torch.matmul(x.t(),  x)
+    matB =  torch.matmul(x.t(), y)
+    est = torch.matmul(torch.pinverse(matA), matB)
+    return est
 # -------------------------------------------------------------------------------------------
 #  # Main Script
 # -------------------------------------------------------------------------------------------
@@ -116,22 +127,22 @@ def run_task_learner(trainData, priorMu, priorVar):
 # priorsSetMu = np.linspace(0.0, 10.0, nPriors)
 priorsSetMu = np.array([0.0, 1.0, 2.0, 3.0, 4.0, 5.0])
 nPriors = priorsSetMu.shape[0]
-priorVar = 0.1**2  # TODO: make prior variance different so it will help the results which the prior is correct
+priorVar = 0.2**2  # TODO: make prior variance different so it will help the results which the prior is correct
 postVar = priorVar
 
 cumulativeBound = np.zeros(nPriors)
 
 taskEnv = TaskEnvironment()
 
-n_samples = 3 # number of samples per task TODO: draw at random for each task
+n_samples = 2 # number of samples per task TODO: draw at random for each task
 # -------------------------------------------------------------------------------------------
 #   Main lifelong learning loop
 # -------------------------------------------------------------------------------------------
-T = 50  # number of tasks
+T = 100  # number of tasks
 for t in range(T):
     # generate task
     task = taskEnv.generate_task()
-    print(task.a)
+    # print(task.a)
     trainData = task.get_samples(n_samples)
     # trainData.plot()
     for i_prior in range(nPriors):
@@ -144,7 +155,7 @@ for t in range(T):
 # hyper-posterior calculation
 # -------------------------------------------------------------------------------------------
 
-print([(priorsSetMu[k], cumulativeBound[k]) for k in range(nPriors)])
+print([(priorsSetMu[k], cumulativeBound[k] / T) for k in range(nPriors)])
 
 hyperPrior = np.ones(nPriors) / nPriors
 alpha = 1 / np.sqrt(T) + 1 / n_samples  # assuming all tasks have the same number of samples
@@ -159,16 +170,40 @@ print('Transfer Bound: {}'.format(transferBound))
 # meta-testing
 # -------------------------------------------------------------------------------------------
 nReps = 100
-errVec = np.zeros(nReps)
+errVecAlg = np.zeros(nReps)
+errVec0prior = np.zeros(nReps)
+errVecNoPrior = np.zeros(nReps)
+errVecAlgPeak = np.zeros(nReps)
+
 for iRep in range(nReps):
     # draw prior from hyper-posterior
     priorMu = np.random.choice(priorsSetMu, 1, p=hyperPosterior)[0]
     # generate task
     task = taskEnv.generate_task()
     trainData = task.get_samples(n_samples)
+    # Check expected error when using learned hyper-posterior
     postMu, taskBound = run_task_learner(trainData, priorMu, priorVar)
-    # Check expected error
-    errVec[iRep] = task.est_test_error(postMu, postVar)
-print('Estimated transfer-error: {}'.format(errVec.mean()))
+    errVecAlg[iRep] = task.est_test_error(postMu, postVar)
+
+    # Check expected error when using learned hyper-posterior
+    postMu, taskBound = run_task_learner(trainData, priorMu, priorVar)
+    errVecAlgPeak[iRep] = task.est_test_error(postMu, 0)
+
+    # Check expected error when using prior #0
+    postMu, taskBound = run_task_learner(trainData, priorsSetMu[0], priorVar)
+    errVec0prior[iRep] = task.est_test_error(postMu, postVar)
+
+    # Check expected error when using no prior
+    est_a = run_no_prior_learner(trainData)
+    errVecNoPrior[iRep] = task.est_test_error(est_a, 0)
+
+
+print('Estimated transfer-error, using Lifelong Alg: {}'.format(errVecAlg.mean()))
+print('Estimated transfer-error, using Lifelong Alg +peak-posterior: {}'.format(errVecAlgPeak.mean()))
+print('Estimated transfer-error, using prior #0: {}'.format(errVec0prior.mean()))
+print('Estimated transfer-error, not using prior: {}'.format(errVecNoPrior.mean()))
+
+
+
 
 
